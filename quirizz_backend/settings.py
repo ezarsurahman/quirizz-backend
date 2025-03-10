9aad29440b363cfaf99d4ef1c70c1102fa582e32
Ezar Surahman <akhdanezar@gmail.com>
2025-03-10 19:53:04 +0700

    build: add railway deployment

--- quirizz_backend/settings.py
+++ quirizz_backend/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xrz4@x==2*2h^xek18z7iizjjc!c+tuvi5@f2o$pr7al#%$5)_'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY",'django-insecure-xrz4@x==2*2h^xek18z7iizjjc!c+tuvi5@f2o$pr7al#%$5)_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app',]
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.railway.app",
+    "https://*.railway.app",
+]
 
 
 # Application definition
